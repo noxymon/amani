5f5b5d5be6ef9b7d743a85d4c7a4f8200d2b991b
noxymon <avicienna.ulhaq@gmail.com>
2020-05-12 05:20:46 +0700

    add project structure

---
+++ src/main/java/id/akademi/amanda/AmandaApplication.java
@@ -0,0 +1,15 @@
+package id.akademi.amanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmandaApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AmandaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/akademi/amanda/AmandaApplicationTests.java
@@ -0,0 +1,15 @@
+package id.akademi.amanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmandaApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
